4cb14bd1b4bdfcbdab7c46c4917c6488a2f2bbe9
Alex Marriott <alex.marriott@buildcircle.co.uk>
2021-10-20 09:52:22 +0100

    basic connection between server and client established

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+
+  render() {
+    return (
+        <div>
+          Practical Intro To WebSockets.
+        </div>
+    );
+  }
+}
+
+export default App;

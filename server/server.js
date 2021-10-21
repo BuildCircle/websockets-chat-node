0e11571a94386492a08a6e9802fdbe8a94ae1960
Pawel Dabkowski <pawel.dabkowski@buildcircle.co.uk>
2021-10-21 10:38:29 +0100

    server test

--- server/server.js
+++ server/server.js
@@ -1,52 +1,76 @@
 const webSocketsServerPort = 8000;
-const webSocketServer = require('websocket').server;
-const http = require('http');
+const webSocketServer = require("websocket").server;
+const http = require("http");
 
-const server = http.createServer();
-server.listen(webSocketsServerPort);
-const wsServer = new webSocketServer({
-    httpServer: server
-});
+const startServer = () => {
+  const server = http.createServer();
+  server.listen(webSocketsServerPort);
+  const wsServer = new webSocketServer({
+    httpServer: server,
+  });
+  console.log("I am here");
 
-const clients = {};
-let text = '';
-let users = {};
+  const clients = {};
+  let text = "";
+  let users = {};
 
-const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    return s4() + s4() + '-' + s4();
-};
+  const getUniqueID = () => {
+    const s4 = () =>
+      Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    return s4() + s4() + "-" + s4();
+  };
 
-const sendMessage = (json) => {
+  const sendMessage = (json) => {
     Object.keys(clients).map((client) => {
-        clients[client].sendUTF(json);
+      clients[client].sendUTF(json);
     });
-}
+  };
 
-wsServer.on('request', function(request) {
+  wsServer.on("request", function (request) {
     var userID = getUniqueID();
-    console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+    console.log(
+      new Date() +
+        " Recieved a new connection from origin " +
+        request.origin +
+        "."
+    );
     const connection = request.accept(null, request.origin);
     clients[userID] = connection;
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
+    console.log(
+      "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
+    );
 
-    sendMessage(JSON.stringify({message: text}))
+    sendMessage(JSON.stringify({ message: text }));
 
-    connection.on('message', function(message) {
-        object = JSON.parse(message.utf8Data)
-        if (object.type === 'user login') {
-            users[userID] = object.username
-            sendMessage(JSON.stringify({users: Object.values(users), type: 'user login'}))
-        } else if (object.type === 'content change') {
-            text = object.message
-            sendMessage(JSON.stringify({message: text, type: 'content change'}))
-        }
-    })
+    connection.on("message", function (message) {
+      object = JSON.parse(message.utf8Data);
+      console.log("abc", message);
+      if (object.type === "user login") {
+        users[userID] = object.username;
+        sendMessage(
+          JSON.stringify({ users: Object.values(users), type: "user login" })
+        );
+      } else if (object.type === "content change") {
+        text = object.message;
+        // sendMessage(JSON.stringify({message: text, type: 'content change'}))
+      }
+    });
 
-    connection.on('close', function(connection) {
-        console.log((new Date()) + " Peer " + userID + " disconnected.");
-        delete clients[userID];
-        delete users[userID];
-        sendMessage(JSON.stringify({users: Object.values(users), type: 'user login'}));
-     });
-});
+    connection.on("close", function (connection) {
+      console.log(new Date() + " Peer " + userID + " disconnected.");
+      delete clients[userID];
+      delete users[userID];
+      sendMessage(
+        JSON.stringify({ users: Object.values(users), type: "user login" })
+      );
+    });
+  });
+
+//   return server;
+};
+
+startServer();
+
+module.exports = startServer;
--- server/server.test.js
+++ server/server.test.js
@@ -1,18 +1,50 @@
-const server = require('./server');
-const io = require('socket.io-client')
+// const startServer =  require('./server');
+var W3CWebSocket = require("websocket").client;
+var client = new W3CWebSocket();
 
-describe('server', () => {
-    it('request', async () => {
-        new Promise((resolve) => {const client = io("ws://127.0.0.1:8000");
-        client.send(
+describe("server", () => {
+
+  beforeAll(async () => {
+
+    client.on("connect", () => {
+      console.log("WE ARE CONNECTED!")
+    })
+
+    client.connect("ws://127.0.0.1:8000");
+  })
+
+  it("request", () => {
+   
+    // client.send(
+    //   "request",
+    //   JSON.stringify({
+    //     type: "content change",
+    //     message: "abcd",
+    //   })
+    // );
+    // expect.assertions(1)
+    console.log("1st console log", client);
+    new Promise((resolve) => {
+      const timer = setInterval(() => {
+        console.log(client);
+        if (client.readyState === 1) {
+          client.send(
+            "request",
             JSON.stringify({
               type: "content change",
-              message: 'abcd',
+              message: "abcd",
             })
-          ).on('message', (message) => {
-              console.log(message)
-              expect(message).toBe('abcd')
-              resolve();
-          })})
-    })
+          );
+          clearInterval(timer);
+          resolve(client);
+        }
+      }, 1000);
+
+      // .on('message', (message) => {
+      //     console.log(message)
+      //     expect(message).toBe('abcd')
+      //     resolve();
+      // })
+    });
+  });
 });
